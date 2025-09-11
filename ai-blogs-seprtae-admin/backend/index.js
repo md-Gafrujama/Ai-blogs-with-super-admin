cbbaf574f42c05d48d8fa2bc1997b06e0b5fb5b7
Rizwan <mdrizwan6386@gmail.com>
2025-09-11 17:26:34 +0530

    resolved cors error

--- ai-blogs-seprtae-admin/backend/index.js
+++ ai-blogs-seprtae-admin/backend/index.js
@@ -16,7 +16,13 @@ const app = express();
 //   console.log("redis connected successfully");
 // })
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
